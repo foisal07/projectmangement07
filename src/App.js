6d1863f5c3275eeb4907c1a28d47f316ea142de7
Foisal <workkaj01@gmail.com>
2021-11-10 12:51:15 +0600

    Create pages set route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { firestore } from "./firbase/config";
 
+import "./App.css";
+
+// Pages and components
+import Dashboard from "./pages/dashboard/Dashboard";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import CreateProject from "./pages/project/CreateProject";
+import ProjectDetail from "./pages/project/ProjectDetail";
+
 function App() {
-  console.log(firestore);
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="container">
+          <Switch>
+            <Route exact path="/">
+              <Dashboard />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+            <Route path="/project:id">
+              <ProjectDetail />
+            </Route>
+            <Route path="/create">
+              <CreateProject />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/project/ProjectDetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ProjectDetail() {
+    return (
+        <div>
+            <p>Project Detail</p>
+        </div>
+    )
+}
