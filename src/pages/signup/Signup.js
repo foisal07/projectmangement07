24f9835b22c2514bbbe4164e0b19c9cd848dad2c
Foisal <workkaj01@gmail.com>
2021-11-10 14:27:21 +0600

    Handle thumbnail upload

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -8,11 +8,38 @@ export default function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [thumbnail, setThumbnail] = useState("");
+  const [thumbnailError, setThumbnailError] = useState(null);
 
-  const handleFilechange = (e) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, thumbnail);
+  };
+
+  const handleFilechange = (e) => {
+    setThumbnailError(null);
+
+    let selectedFile = e.target.files[0];
+
+    if (!selectedFile) {
+      setThumbnailError("Please select a file");
+    }
+
+    if (!selectedFile.type.includes("image")) {
+      setThumbnailError("Please select an image");
+      return;
+    }
+
+    if (selectedFile.size > 100000) {
+      setThumbnailError("File size must be less than 100kb");
+      return;
+    }
+
+    setThumbnailError(null);
+    setThumbnail(selectedFile);
+  };
 
   return (
-    <form className="auth-form">
+    <form className="auth-form" onSubmit={handleSubmit}>
       <h2>Sign Up</h2>
       <label>
         <span>Name</span>
@@ -22,15 +49,7 @@ export default function Signup() {
           value={name}
         />
       </label>
-      <label>
-        <span>Name</span>
-        <input
-          required
-          type="text"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-        />
-      </label>
+
       <label>
         <span>Email</span>
         <input
@@ -51,11 +70,16 @@ export default function Signup() {
       </label>
       <label>
         <span>Upload profile photo</span>
-        <input
-          type="file"
-        />
+        <input type="file" required onChange={handleFilechange} />
       </label>
-      <button className="btn">Sign Up</button>
+
+      {thumbnailError && (
+        <button className="btn" disabled>
+          Sign Up
+        </button>
+      )}
+      {!thumbnailError && <button className="btn">Sign Up</button>}
+      {thumbnailError && <div className="error">{thumbnailError}</div>}
     </form>
   );
 }
