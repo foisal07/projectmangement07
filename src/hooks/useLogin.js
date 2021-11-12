4a8f5bd60423ca2b48a34bb2bb39b17ef3861016
Foisal <workkaj01@gmail.com>
2021-11-12 13:01:23 +0600

    Login user

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,52 +1,47 @@
-import { auth } from "../firebase/config.js";
+import { auth } from "../firbase/config";
 import { useState, useEffect } from "react";
-import {useAuthContext} from './useAuthContext'
+import { useAuthContext } from "./useAuthContext";
 
 const useLogin = () => {
-const [isCancelled, setIsCancelled] = useState(false);
+  const [isCancelled, setIsCancelled] = useState(false);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const {dispatch} = useAuthContext()
+  const { dispatch } = useAuthContext();
 
-
- const login = async (email, password) => {
+  const login = async (email, password) => {
     // remove previous error
     setError(null);
     setIsLoading(true);
 
     try {
       // Login
-      const response = await auth.signInWithEmailAndPassword(email, password)
+      const response = await auth.signInWithEmailAndPassword(email, password);
 
       if (!response) {
         throw new Error("Something went wrong!");
       }
 
-    console.log(response.user);
-      // dispatch action to update login user local state 
-      dispatch({type:'LOGIN', payload: response.user})
+      // dispatch action to update login user local state
+      dispatch({ type: "LOGIN", payload: response.user });
 
-      if(!isCancelled){
-    setIsLoading(false);
-      setError(null);
+      if (!isCancelled) {
+        setIsLoading(false);
+        setError(null);
       }
-
-      
     } catch (err) {
-    console.log(err.message);
-    if(!isCancelled){
-      setError(err.message);
-      setIsLoading(false);
-    }
-
+      console.log(err.message);
+      if (!isCancelled) {
+        setError(err.message);
+        setIsLoading(false);
+      }
     }
   };
 
   useEffect(() => {
-      return () => {
-          setIsCancelled(true)
-      }
-  }, [])
+    return () => {
+      setIsCancelled(true);
+    };
+  }, []);
 
   return {
     error,
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from "react";
+import useLogin from "../../hooks/useLogin";
+import "./Login.css";
 
 export default function Login() {
-    return (
-        <div>
-            <p>Login</p>
-        </div>
-    )
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const { login, isLoading, error } = useLogin();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(email, password);
+  };
+  return (
+    <form className="login-form" onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <label>
+          <span>Email</span>
+          <input
+            type="text"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+
+        <span>Password</span>
+        <input
+          type="password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+
+      {!isLoading && (
+        <button className="btn" onClick={handleSubmit}>
+          Login
+        </button>
+      )}
+
+      {isLoading && (
+        <button className="btn" disabled>
+          Logging In...
+        </button>
+      )}
+
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
 }
