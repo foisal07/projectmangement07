8b1d09cf8b19c1154f4b36fb2f18e1b9baa41eaf
Foisal <workkaj01@gmail.com>
2021-11-12 14:06:28 +0600

    Show avatar, hide sidebar on logout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import useLogout from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 // styles
 import "./Navbar.css";
@@ -8,6 +9,7 @@ import Temple from "../assets/temple.svg";
 
 export default function Navbar() {
   const { logout, isLoading } = useLogout();
+  const { user } = useAuthContext();
   return (
     <nav className="navbar">
       <ul>
@@ -15,24 +17,31 @@ export default function Navbar() {
           <img src={Temple} alt="dojo logo" />
           <span>Managey</span>
         </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          {!isLoading && (
-            <button className="btn" onClick={logout}>
-              Logout
-            </button>
-          )}
-          {isLoading && (
-            <button className="btn" disabled>
-              Logging out
-            </button>
-          )}
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+
+        {user && (
+          <li>
+            {!isLoading && (
+              <button className="btn" onClick={logout}>
+                Logout
+              </button>
+            )}
+            {isLoading && (
+              <button className="btn" disabled>
+                Logging out
+              </button>
+            )}
+          </li>
+        )}
       </ul>
     </nav>
   );
