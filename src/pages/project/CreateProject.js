403934dbb0b9d98196d4721c2875590cbcb1ba73
Foisal <workkaj01@gmail.com>
2021-11-13 12:52:02 +0600

    Create Add Project component

--- src/pages/project/CreateProject.js
+++ src/pages/project/CreateProject.js
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useState } from "react";
+import Select from "react-select";
+
+const categories = [
+  { value: "development", label: "Development" },
+  { value: "design", label: "Design" },
+  { value: "sales", label: "Sales" },
+  { value: "marketing", label: "Marketing" },
+];
 
 export default function CreateProjects() {
-    return (
-        <div>
-           <p>create project</p> 
-        </div>
-    )
+  const [name, setName] = useState("");
+  const [details, setDetails] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [category, setCategory] = useState("");
+  const [assignedUsers, setAssignedUsers] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, details, dueDate, category.value);
+  };
+
+  return (
+    <form>
+      <h2>Add project</h2>
+      <label>
+        <span>Title:</span>
+        <input
+          required
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+      </label>
+
+      <label>
+        <span>Details:</span>
+        <input
+          required
+          type="text"
+          onChange={(e) => setDetails(e.target.value)}
+          value={details}
+        />
+      </label>
+
+      <label>
+        <span>Due date:</span>
+        <input
+          required
+          type="date"
+          onChange={(e) => setDueDate(e.target.value)}
+          value={dueDate}
+        />
+      </label>
+
+      <label>
+        <span>Pick category:</span>
+        <Select
+          onChange={(option) => setCategory(option)}
+          options={categories}
+        />
+      </label>
+
+      {/* <label>
+        <span>Assign to:</span>
+        <input
+          required
+          type="text"
+          onChange={(e) => setAssignedUsers(e.target.value)}
+          value={assignedUsers}
+        />
+      </label> */}
+      <button className="btn" onClick={handleSubmit}>Add Project</button>
+    </form>
+  );
 }
